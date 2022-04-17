6d2c802119609a4af3a9627270dddc810c403acf
Luis Alves <coopercontato27@gmail.com>
2022-04-17 20:32:57 -0300

    adicionando exercício 7 do dia 4, bloco 4

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/parte-1.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/parte-1.js
@@ -61,4 +61,14 @@ let leitor = {
   ],
 };
 
-console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '".')
+// console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '".')
+
+// Exercício 07
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+});
+
+console.log(leitor)
