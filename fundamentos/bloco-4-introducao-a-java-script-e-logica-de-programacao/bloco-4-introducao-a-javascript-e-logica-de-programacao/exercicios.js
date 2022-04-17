6833d03801bc87e5ab3b52b49f5a032929ee3192
Luis Alves <coopercontato27@gmail.com>
2022-04-17 20:04:13 -0300

    adicionando exercício 2 do dia 4, bloco 4

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicios.js
@@ -1,7 +1,14 @@
+// Exercício 01
+
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log('Bem-vinda, ' + info.personagem)
+// console.log('Bem-vinda, ' + info.personagem);
+
+// Exercício 02
+
+info['recorrente'] = 'Sim';
+console.log(info);
