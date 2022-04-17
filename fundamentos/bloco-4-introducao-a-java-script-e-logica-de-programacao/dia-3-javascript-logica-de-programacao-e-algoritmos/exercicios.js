cd53cd52aac01af22516095a4dc5e255de4fa78f
Luis Alves <coopercontato27@gmail.com>
2022-04-17 17:00:09 -0300

    adicionando algoritmo 2 do dia 3 bloco 4

--- fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,5 +1,7 @@
+// Algoritmo 01
+
 // Criar uma vairável que recebe o número a ser fatorado
-let number = 4;
+let number = 10;
 // Criar uma array contendo todos os numeros que o loop encontrar, contendo o numero em questão
 let decrement = [number];
 // Criar um loop que irá decrementar o valor da variável em questão, até que o o velor seja igual a '1'
@@ -13,4 +15,19 @@ for (num of decrement){
   result *= num;
 }
 // Criar um console.log para que retorne o valor da variável resultado
-console.log(result);
+// console.log(result);
+
+// -------------------------------------------------------------------------
+
+// Algoritmo 02
+
+// Criar uma variável que irá guardar a palavra que será invertida
+let word = 'tryber';
+// Criar uma variável que ira guardar o resultado do meu loop que inverte a palavra
+let invertedWord = '';
+//Criar um loop que busca cada letra da minha palavra e concatena com a variavel da palavra invertida
+for (index in word){
+  invertedWord = word[index] + invertedWord;
+}
+// Criar um console.log para revelar a palavra invertida
+console.log(invertedWord);
