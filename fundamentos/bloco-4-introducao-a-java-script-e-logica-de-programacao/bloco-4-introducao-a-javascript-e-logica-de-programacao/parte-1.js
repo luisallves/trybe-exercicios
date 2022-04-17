cdb0cb131aea830d63700378fd4109aba3d2d422
Luis Alves <coopercontato27@gmail.com>
2022-04-17 20:21:37 -0300

    adicionando exercício 5 do dia 4, bloco 4

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/parte-1.js
@@ -0,0 +1,47 @@
+// Exercício 01
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// console.log('Bem-vinda, ' + info.personagem);
+
+// Exercício 02
+
+info['recorrente'] = 'Sim';
+// console.log(info);
+
+
+
+for (key in info) {
+  // console.log(key);
+};
+
+// Exercício 04
+
+for (key in info) {
+  // console.log(info[key]);
+};
+
+// Exercício 05
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell´s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente:'Sim'
+};
+
+for (key in info) {
+  if (
+    key === 'recorrente' &&
+    info[key] === 'Sim' &&
+    info2[key] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
+};
