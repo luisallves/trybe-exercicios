197c3c7e262bd6eddd194e95c96cb86aed07a5c0
Luis Alves <coopercontato27@gmail.com>
2022-05-24 15:56:45 -0300

    exercício 3 dia 8.1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -45,4 +45,31 @@ const callback = (numEsc, numSort) => {
     } else {console.log("Tente novamente");};
 };
 
-resultadoSorteio(5,callback);
+// resultadoSorteio(5,callback);
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const gabarito = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const respostas = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+let nota = 0
+
+const correcao = (gabarito, respostas, acao) => {
+    for (let i = 0; i < gabarito.length; i += 1){
+        let conta = acao(gabarito[i], respostas[i])
+        nota += conta;
+    }
+    console.log(nota);
+};
+
+const acao = (gabarito, resposta) => {
+    if (resposta === gabarito) {
+        return 1;
+    } else if (resposta === 'N.A') {
+        return 0;
+    } else {
+        return -0.5;
+    }
+};
+
+correcao(gabarito, respostas, acao);
