21a69cec22fcc102f5857c052d8c920ab2f579b4
Luis Alves <coopercontato27@gmail.com>
2022-05-24 15:34:59 -0300

    exercício 2 dia 8.1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -28,4 +28,21 @@ const emailName = (fullName) => {
     return email;
 };
 
-console.log(person('Luis Fernando Alves Pereira'));
+// console.log(person('Luis Fernando Alves Pereira'));
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const resultadoSorteio = (numEsc, callback) => {
+const sorteio = Math.floor(Math.random() * (6 - 1) + 1);
+callback(numEsc, sorteio);
+};
+
+const callback = (numEsc, numSort) => {
+    console.log(numSort)
+    console.log(numEsc)
+    if (numEsc === numSort) {
+        console.log("Parabéns você ganhou");
+    } else {console.log("Tente novamente");};
+};
+
+resultadoSorteio(5,callback);
