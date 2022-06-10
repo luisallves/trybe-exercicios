b845314b5cc7d50279fb98473a9bfe3d1fb80381
Luis Alves <coopercontato27@gmail.com>
2022-06-10 20:09:17 -0300

    emplementando o exercício 3 e 4

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-10-1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-10-1/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Compromissos from './Components/Compromissos';
 import './Components/Task';
 import Task from './Components/Task';
+import Content from './Content';
+import Footer from './Footer';
+import Header from './Header';
 
 const App = () =>{
   return(
     <>
+      <Header/>
       {Task('Luis')}
       <Compromissos/>
+      <Content/>
+      <Footer/>
     </>
   )
 }
